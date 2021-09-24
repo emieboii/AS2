ce826741903f130fc820a04b08c70b99c9ae018a
Emil Gullaksen <emie@live.dk>
2021-09-24 11:24:07 +0200

    Update StatusController.cs

--- AS2/StatusController.cs
+++ AS2/StatusController.cs
@@ -4,6 +4,13 @@ namespace AS2
 {
     public class StatusController
     {
-        
+        public Status IsNewbie(DateTime startDate)
+        {
+            var studentTimeSpan = (Datetime.Now.Subtract(startDate)
+            if (studentTimeSpan < 60 && studentTimeSpan > 0) => Status.Active;
+            
+
+        }
+
     }
 }
