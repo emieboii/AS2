a4b90f4e30873e40c981435dec9f562feb2c2581
Emil Gullaksen <emie@live.dk>
2021-09-28 11:51:38 +0200

    testing StatusController

--- AS2.Tests/UnitTest1.cs
+++ AS2.Tests/UnitTest1.cs
@@ -1,4 +1,6 @@
 using System;
+using static AS2.Status;
+using static AS2.StatusController;
 using Xunit;
 
 namespace AS2.Tests
@@ -23,6 +25,48 @@ namespace AS2.Tests
 
             var expected = -1;
             Assert.Equal(expected, student.Id);
+        }
+
+        [Fact]
+        public void StatusController_Status_returns_New_when_totalDays_lower_than_0()
+        {
+            var controller = new StatusController();
+
+            DateTime started = new DateTime(2021, 9, 29, 0, 0 ,0);
+            DateTime today   = new DateTime(2021, 9, 28, 0, 0 ,0);
+
+            var exp = Status.New;
+            var result = controller.status(started, today);
+
+            Assert.Equal(exp, result);
+        }
+
+        [Fact]
+        public void StatusController_Status_returns_Active_when_totalTime_less_than_3_years()
+        {
+            var controller = new StatusController();
+
+            DateTime started = new DateTime(2021, 9, 29, 0, 0 ,0);
+            DateTime today   = new DateTime(2021, 10, 28, 0, 0 ,0);
+
+            var exp = Status.Active;
+            var result = controller.status(started, today);
+
+            Assert.Equal(exp, result);
+        }
+
+        [Fact]
+        public void StatusController_Status_returns_Graduated_when_totalTime_above_3_years()
+        {
+            var controller = new StatusController();
+
+            DateTime started = new DateTime(2021, 9, 29, 0, 0 ,0);
+            DateTime today   = new DateTime(2025, 10, 28, 0, 0 ,0);
+
+            var exp = Status.Graduated;
+            var result = controller.status(started, today);
+
+            Assert.Equal(exp, result);
         }
     }
 }
--- AS2/StatusController.cs
+++ AS2/StatusController.cs
@@ -1,16 +1,32 @@
 using System;
+using static AS2.Status;
 
 namespace AS2
 {
     public class StatusController
     {
-        public Status IsNewbie(DateTime startDate)
-        {
-            var studentTimeSpan = (Datetime.Now.Subtract(startDate)
-            if (studentTimeSpan < 60 && studentTimeSpan > 0) => Status.Active;
+        public Status status(DateTime startDate, DateTime currentDate)
+        {   
+            var studentTimeSpan = currentDate.Subtract(startDate);
             
-
+            if (studentTimeSpan.TotalHours < yearToHours(3) && studentTimeSpan.Days > 0)
+            {
+                return Status.Active;
+            }
+            if (studentTimeSpan.TotalHours > yearToHours(3))
+            {
+                return Status.Graduated;
+            }
+            if (studentTimeSpan.TotalDays < 0)
+            {
+                return Status.New;
+            }
+            return Status.Dropout;
         }
 
+        public int yearToHours(int years)
+        {
+            return 24 * 365 * years;
+        }
     }
 }
